d76f4256e08d964a0cbf1c79c15a46848a43ae5f
Michael McCouman jr. <support@wikibyte.org>
2013-11-22 22:51:30 +0100

    Update single-podunion.php

--- single-podunion.php
+++ single-podunion.php
@@ -20,8 +20,8 @@ echo '<article>
 ?>
 
 
-	<div id="primary" class="site-content">
-		<div id="content" role="main">
+<div id="primary" class="site-content">
+<div id="content" role="main">
 <div class="entry-content">
 
 			<?php while ( have_posts() ) : the_post(); ?>
@@ -143,12 +143,7 @@ echo $rico_hls['highlights_rico_time'].' Die Highlights von Rico <http://podunio
 if ($torsten_hls){
 echo $torsten_hls['highlights_torsten_time'].' Die Highlights von Torsten <http://podunion.com/?p='.$angaben['podunion_magazin_postid'].'>' . "\n";
 }
-/*
-//HT Gast (1)
-if ($gasteins_hls['highlights_gasteins_username']){
-echo $gasteins_hls['highlights_gasteins_time'].' Die Highlights von '. $gasteins_hls['highlights_gasteins_username']. ' <http://podunion.com/?p='.$angaben['podunion_magazin_postid'].'>' . "\n";
-}
-*/
+
 echo $magend['podunion_magazin_ende_time'].' Verabschiedung <http://podunion.com/?p='.$angaben['podunion_magazin_postid'].'>' . "\n";
 
 echo '</textarea>
@@ -156,34 +151,6 @@ echo '</textarea>
 <script src="http://cm.wikibyte.org/wp-content/themes/podunion-magazin/box/magazin/js/jquery.generateFile.js"></script>
 <script src="http://cm.wikibyte.org/wp-content/themes/podunion-magazin/box/magazin/js/script.js"></script>      
 </form><br><br>';
-
-
-/*
-##Ausgabe Bilder:
-echo '<h2>Bilder Downloads</h2>';
-
-echo '<h5>Titelbild:</h5><br>
-<img style="width:120px;" src="'.$angaben['podunion_magazin_cover'].'">  
-<a href="'.$angaben['podunion_magazin_cover'].'">Download</a>
-<br>';
-
-echo '<h5>Titelbild:</h5><br>
-<img style="width:120px;" src="'.$angaben['podunion_magazin_cover'].'">  
-<a href="'.$angaben['podunion_magazin_cover'].'">Download</a>
-<br>';
-*/
-
-
-
-
-///----------------------------/Auphonic--------------------------------------------            
-
-
-
-
-
-
-
 
 ///#################################### HTML DATEI der PODUNION ####################################           
 
@@ -204,7 +171,6 @@ echo '&amp;nbsp;'. "\n\n";
 
 echo '<h2><a href="http://podunion.com/?p='.$angaben['podunion_magazin_postid'].'" title="Show Notes">Show Notes</a></h2>' . "\n\n";
 ///----------------------------/Start--------------------------------------------            
-
 
 
 ///----------------------------Begrüssung--------------------------------------------            
@@ -229,25 +195,6 @@ echo '<h3>Die Themen dieser Folge</h3>'. "\n";
 echo '<ul>'. "\n";
 echo '	<li><a href="#begruessung">Begrüssung</a></li>'. "\n";
 echo '	<li><a href="#feedback">Feedbacks zu den letzten Sendungen</a></li>'. "\n";
-/*
-//Unsere Gäste
-if ($gasteins_hls){
-
-	//Einzahl/Mehrzahl
-	if ($gasteins_hls){
-		echo '	<li><a href="#zugast">Unser Gast</a>'. "\n";
-	} else {
-		echo '	<li><a href="#zugast">Unsere Gäste</a>'. "\n";
-	}
-
-echo '		<ul>'. "\n";
-
-	//Gast (1)
-	echo '			<li><a href="#'.preg_replace('/ +/', '',  strtolower($gasteins_hls['highlights_gasteins_username'])).'">'.$gasteins_hls['highlights_gasteins_username'].'</a></li>'. "\n";
-
-echo '		</ul>'. "\n";
-echo '	</li>'. "\n";
-}*/
 echo '	<li><a href="#'. preg_replace('/ +/', '',  strtolower($themenwahl['magazin_thema'][0]['magazin_thema_header'])). '">Unser Themen</a>'. "\n";
 echo '		<ul>'. "\n";
 
@@ -260,12 +207,7 @@ echo '		</ul>'. "\n";
 echo '	</li>'. "\n";
 echo '	<li><a href="#highlights">Unserer Podcast Highlights</a>'. "\n";
 echo '		<ul>'. "\n";
-/*
-//HT Gast(1)
-if ($gasteins_hls){
-	echo '			<li><a href="#'. preg_replace('/ +/', '',  strtolower($gasteins_hls['highlights_gasteins_username'])) .'">Die Highlights von '. $gasteins_hls['highlights_gasteins_username'] .'</a></li>'. "\n";
-}
-*/
+
 //HT Jorg
 if ($j_hls){ 
 	echo '			<li><a href="#joerg">Die Highlights von Jörg</a></li>'. "\n";
@@ -313,37 +255,6 @@ echo '<div style="text-align: right;"><a href="#themen">&amp;nbsp;&amp;uarr;&amp
 
 echo '&amp;nbsp;' . "\n\n"; 
 ///----------------------------/Feedbacks--------------------------------------------
-
-
-
-
-
-///--- Zu Gast in der PudUnion --------------
-/*
-//Gast (1)
-if ($gasteins_hls['highlights_gasteins'][0]['highlights_gasteins_name']){
-	
-	echo '<h3><a name="zugast"></a>Unser Gast</h3>'. "\n\n";
-		
-	echo '<a href="'.preg_replace('/ +/', '',  strtolower($gasteins_hls['highlights_gasteins_username'])).'"></a>'. "\n\n";
-
-	echo '[raw][one_third]<a href="' . $gasteins_hls['highlights_gasteins_userurl'] . '" title="Zu Gast im Podunion Magazin: ' . $gasteins_hls['highlights_gasteins_username'].'" target="_blank"><img src="'.$gasteins_hls['highlights_gasteins_avatar'].'" alt="'.$gasteins_hls['highlights_gasteins_username'].'" width="300" height="300" class="aligncenter size-medium wp-image-6353" /></a>[/one_third][two_third last]'. $gasteins_hls['highlights_gasteins_about']. "\n";
-	echo '<ul>'."\n";
-	echo '  	<li><a href'.$gasteins_hls['highlights_gasteins_userurl'].'>Webpage</a></li>'. "\n";
-	echo '</ul>'. "\n";
-	echo '[/two_third][clear][/raw]'. "\n";	
-	echo '<div style="text-align: right;"><a href="#themen">&amp;nbsp;&amp;uarr;&amp;nbsp;</a></div>' . "\n\n";
-
-	echo '&amp;nbsp;' . "\n\n"; 
-}*/
-///-------------------------------------------
-
-
-
-
-
-
-
 
 
 ///----------------------------Themen der Episode-----------------------------------
@@ -419,29 +330,6 @@ if ($themenwahl['magazin_thema'][0]['magazin_thema_header']){
 
 echo '<h2>Unsers Highlights <a name="highlights"></a></h2>' . "\n\n"; 
 
-################ Gast (1) HTs #####################################
-/*
-if ($gasteins_hls){
-echo '&amp;nbsp;' . "\n\n";
-
-echo '<a name="'.preg_replace('/ +/', '',  strtolower($gasteins_hls['highlights_gasteins_username'])).'"></a><a href="#t=' . $gasteins_hls['highlights_gasteins_time'] .'">' . substr($j_hls['highlights_gasteins_time'], 0, -7) . '</a>' . "\n\n";
-
-echo '<h3>Die Highlights von '; 
-
-	if($gasteins_hls['highlights_gasteins_userurl']){
-		echo '<a href="'.$gasteins_hls['highlights_gasteins_userurl'].'" title="'.$gasteins_hls['highlights_gasteins_username'].'">'.$gasteins_hls['highlights_gasteins_username'].'</a></h3>'. "\n";
-	} else {
-		echo $gasteins_hls['highlights_gasteins_username']. '</h3>'. "\n";
-	}
-
-	foreach ($gasteins_hls['highlights_gasteins'] as $geins_hts){
-                
-    	echo '<strong><a href="' . $geins_hts['highlights_gasteins_link'] . '" title="'.$geins_hts['highlights_gasteins_name'].'" target="_blank">' . $geins_hts['highlights_gasteins_name'] . '</a></strong>'. "\n";
-    	echo '[raw][one_third]<a href="' . $geins_hts['highlights_gasteins_link'] . '" title="'.$geins_hts['highlights_gasteins_name'].'" target="_blank"><img src="'.$geins_hts['highlights_gasteins_image'].'" alt="'. $geins_hts['highlights_gasteins_name'] . '" width="300" height="300" class="aligncenter size-medium wp-image-6353" /></a>[/one_third][two_third last]'. $geins_hts['highlights_gasteins_zusammenfassung'] . '[/two_third][clear][/raw]' . "\n";		  
-		echo '<div style="text-align: right;"><a href="#themen">&amp;nbsp;&amp;uarr;&amp;nbsp;</a></div>'. "\n\n";
-	}
-}
-*/
 ################ Jörg B. HTs #####################################
 if ($j_hls){
 echo '&amp;nbsp;' . "\n\n";
@@ -462,7 +350,7 @@ echo '<h3>Die Highlights von Jörg</h3>'. "\n";
 		if($jorg_hts['highlights_jorg_text_eins']){	
 			echo '<li><a href="' . $jorg_hts['highlights_jorg_link'] . '" title="Link zu Jörgs Highlight: '. $jorg_hts['highlights_jorg_name']. '">'.$jorg_hts['highlights_jorg_name'].'</a></li>'. "\n";
 		}			
-    	//jorgs zusatzlink (1)
+    		//jorgs zusatzlink (1)
 		if($jorg_hts['highlights_jorg_text_eins']){	
 			echo '<li><a href="' . $jorg_hts['highlights_jorg_link_eins'] . '" title="Link zu Jörgs Highlights: '. $jorg_hts['highlights_jorg_name']. '">'.$jorg_hts['highlights_jorg_text_eins'].'</a></li>'. "\n";
 		}
@@ -501,7 +389,7 @@ echo '<h3>Die Highlights von Martin</h3>'. "\n";
 		if($martin_hts['highlights_martin_text_eins']){	
 			echo '<li><a href="' . $martin_hts['highlights_martin_link'] . '" title="Link zu Martins Highlight: '. $martin_hts['highlights_martin_name']. '">'.$martin_hts['highlights_martin_name'].'</a></li>'. "\n";
 		}			
-    	//martins zusatzlink (1)
+    		//martins zusatzlink (1)
 		if($martin_hts['highlights_martin_text_eins']){	
 			echo '<li><a href="' . $martin_hts['highlights_martin_link_eins'] . '" title="Link zu Martins Highlight: '. $martin_hts['highlights_martin_name']. '">'.$martin_hts['highlights_martin_text_eins'].'</a></li>'. "\n";
 		}
@@ -540,7 +428,7 @@ echo '<h3>Die Highlights von michael</h3>'. "\n";
 		if($michael_hts['highlights_michael_text_eins']){	
 			echo '<li><a href="' . $michael_hts['highlights_michael_link'] . '" title="Link zu Michaels Highlight: '. $michael_hts['highlights_michael_name']. '">'.$michael_hts['highlights_michael_name'].'</a></li>'. "\n";
 		}			
-    	//michaels zusatzlink (1)
+		//michaels zusatzlink (1)
 		if($michael_hts['highlights_michael_text_eins']){	
 			echo '<li><a href="' . $michael_hts['highlights_michael_link_eins'] . '" title="Link zu Michaels Highlight: '. $michael_hts['highlights_michael_name']. '">'.$michael_hts['highlights_michael_text_eins'].'</a></li>'. "\n";
 		}
@@ -579,7 +467,7 @@ echo '<h3>Die Highlights von Rico</h3>'. "\n";
 		if($rico_hts['highlights_rico_text_eins']){	
 			echo '<li><a href="' . $rico_hts['highlights_rico_link'] . '" title="Link zu Ricos Highlight: '. $rico_hts['highlights_rico_name']. '">'.$rico_hts['highlights_rico_name'].'</a></li>'. "\n";
 		}			
-    	//ricos zusatzlink (1)
+    		//ricos zusatzlink (1)
 		if($rico_hts['highlights_rico_text_eins']){	
 			echo '<li><a href="' . $rico_hts['highlights_rico_link_eins'] . '" title="Link zu Ricos Highlight: '. $rico_hts['highlights_rico_name']. '">'.$rico_hts['highlights_rico_text_eins'].'</a></li>'. "\n";
 		}
@@ -618,7 +506,7 @@ echo '<h3>Die Highlights von Torsten</h3>'. "\n";
 		if($torsten_hts['highlights_torsten_text_eins']){	
 			echo '<li><a href="' . $torsten_hts['highlights_torsten_link'] . '" title="Link zu Torstens Highlight: '. $torsten_hts['highlights_torsten_name']. '">'.$torsten_hts['highlights_torsten_name'].'</a></li>'. "\n";
 		}			
-    	//torstens zusatzlink (1)
+    		//torstens zusatzlink (1)
 		if($torsten_hts['highlights_torsten_text_eins']){	
 			echo '<li><a href="' . $torsten_hts['highlights_torsten_link_eins'] . '" title="Link zu Torstens Highlight: '. $torsten_hts['highlights_torsten_name']. '">'.$torsten_hts['highlights_torsten_text_eins'].'</a></li>'. "\n";
 		}
@@ -715,16 +603,15 @@ echo '[content_block id=2630]';
 echo '</textarea>';        
 ?>
 
-	<?php endwhile; // end of the loop. ?>
+<?php endwhile; // end of the loop. ?>
 <?php
 
 
 #}
 ?>
 
-	</div>
-	</div><!-- #content -->
-	</div><!-- #primary -->
+</div>
+</div><!-- #content -->
+</div><!-- #primary -->
 
-<?php #get_sidebar(); ?>
 <?php get_footer(); ?>
